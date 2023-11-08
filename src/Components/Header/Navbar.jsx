8bf751d08cabeeb5ef7ee2281b363333732365b5
Sujit Mandal <sm.bright22@gmal.com>
2023-11-08 22:54:42 +0600

    Darkmode setup with toggle button at Navbar

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -11,19 +11,7 @@ const Navbar = () => {
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    const localTheme = localStorage.getItem("theme");
-    document.querySelector("html").setAttribute("data-theme", localTheme);
-  }, [theme]);
-  const handleToggle = (e) => {
-    if (e.target.checked) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
-
+  
   const handleLogOut = () => {
     logOut()
       .then(() => {
