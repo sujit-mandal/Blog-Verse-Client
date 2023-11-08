18876e0f92cb1aeb967c002ff15f418ca6e3af1e
Sujit Mandal <sm.bright22@gmal.com>
2023-11-08 23:22:34 +0600

    Updated readme file with describing key feature

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -11,7 +11,19 @@ const Navbar = () => {
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
-  
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
   const handleLogOut = () => {
     logOut()
       .then(() => {
